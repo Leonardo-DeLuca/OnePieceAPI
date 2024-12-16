74a3adc98287d760dd29dc4a886017833b5726c9
leonardo.luca <leonardo.luca@useall.com.br>
2024-12-16 10:08:25 -0300

    ajuste check ip

--- routes/checkIp.js
+++ routes/checkIp.js
@@ -1,9 +1,10 @@
 // middleware/checkIp.js
-const allowedIp = '172.20.208.1';  // Defina o IP permitido para as rotas POST, PUT, DELETE
+const allowedIp = '177.54.58.34';  // Defina o IP permitido para as rotas POST, PUT, DELETE
 
 // Middleware para verificar o IP da requisição
 const checkIp = (req, res, next) => {
-  const clientIp = req.headers['x-forwarded-for'] || req.ip;
+  const xForwardedFor = req.headers['x-forwarded-for'] || req.ip;
+  const clientIp = xForwardedFor.split(',')[0].trim();
 
   if (clientIp === allowedIp) {
     return next();  // Se o IP for permitido, prossiga com a requisição
